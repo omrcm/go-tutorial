6c819af7a269ffc99019de7f67137783e5d5ed74
Ömür Uçum <omurucum@gmail.com>
2022-06-13 00:08:39 +0300

    Learn Go

---
+++ string.types/string_types.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "Hello, Gopher"
+	fmt.Println(a)
+
+	s := "String with quotes"
+	fmt.Println(s)
+
+	b := `String with back quotes`
+	fmt.Println(b)
+}
