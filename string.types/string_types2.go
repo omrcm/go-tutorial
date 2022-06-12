171a89b2f950b84e7d51e0f5ff5211d035df24ef
Ömür Uçum <omurucum@gmail.com>
2022-06-13 00:18:25 +0300

    Learn Go | New line and Tab in Strings

---
+++ string.types/string_types2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		if you want to put a tab then use \t (backslashe t)
+		if you want to put a new line then use \n (backslashe n)
+	*/
+	a := "Hello \tWorld!"
+	fmt.Println(a)
+
+	var s string = "Hello, \nWorld!"
+	fmt.Println(s)
+}
