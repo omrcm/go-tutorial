d1572fea2b351ac39bad210d3c8ba22b08f41059
Ömür Uçum <omurucum@gmail.com>
2022-06-14 03:49:45 +0300

    Learn Go | String backslashes and merging strings

--- string.types/string_types_backslash.go
+++ string.types/string_types_backslash.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-
 	a := "Hello \"World\" with backslashes!"
 	fmt.Println(a)
 
+	var s1 string = "Hello"
+	var s2 string = "World!"
+	s3 := s1 + " " + s2
+	fmt.Println(s3)
 }
